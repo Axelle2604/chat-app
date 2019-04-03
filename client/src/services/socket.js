3dc6db9f5e50f497b6814496556ac9af536d7dd7
Clemo2604 <axelle.dru@stackerine.com>
2019-04-04 00:11:34 +0200

    Services

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -2,9 +2,34 @@ import socketIOClient from 'socket.io-client';
 
 const socket = socketIOClient('http://localhost:8080');
 
-export const receiveMessage = callback =>
-  socket.on('message', message => callback(message));
+export const serverBroadcastPrevMessages = callback =>
+  socket.on('serverBroadcastPrevMessages', lastMessages =>
+    callback(lastMessages)
+  );
 
-export const sendMessage = message => {
-  socket.emit('newMessage', message);
+export const serverBroadcastAMessage = callback =>
+  socket.on('serverBroadcastAMessage', messageObj => callback(messageObj));
+
+export const chatterSendAMessage = (messageObj, roomName) => {
+  socket.emit('chatterSendAMessage', messageObj, roomName);
+};
+
+export const joinRoom = (roomName, userName) => {
+  socket.emit('joinRoom', roomName, userName);
+};
+
+export const deleteRoom = roomName => {
+  socket.emit('deleteRoom', roomName);
+};
+
+export const createRoom = newRoom => {
+  socket.emit('createRoom', newRoom);
+};
+
+export const updateRoomsTab = cb => {
+  socket.on('updateRoomsTab', roomsTab => cb(roomsTab));
+};
+
+export const userLeavesRoom = (roomName, userName) => {
+  socket.emit('leaveRoom', roomName, userName);
 };
