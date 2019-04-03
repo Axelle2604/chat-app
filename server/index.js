4582f8d25f6bbb36efe3d26a6c3a992fc8ddaa13
Clemo2604 <axelle.dru@stackerine.com>
2019-04-03 23:46:25 +0200

    Server side

--- server/index.js
+++ server/index.js
@@ -8,22 +8,72 @@ const io = socketIo(server);
 
 const sockets = [];
 const lastMessages = [];
+const rooms = new Array(1).fill(1);
 
-broadcast = message => {
-  sockets.forEach(socket => socket.emit('message', message));
+function randomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+function nameRoom(tabRooms) {
+  const number = randomNumber();
+  const isAlreadyExists = tabRooms.find(room => room === number);
+  return !isAlreadyExists ? number : nameRoom(tabRooms);
+}
+
+const createNewRoom = () => {
+  rooms.push(nameRoom(rooms));
+};
+
+const deleteRoom = roomName => {
+  index = rooms.findIndex(room => room === roomName);
+  rooms.splice(index, 1);
+};
+
+const sendMessageToServerAndBroadcastIt = (roomName, messageObj) => {
+  lastMessages.push(messageObj);
+  roomIndex = rooms.indexOf(roomName);
+  io.sockets.in(rooms[roomIndex]).emit('serverBroadcastAMessage', messageObj);
 };
 
 io.on('connection', socket => {
   sockets.push(socket);
-  socket.emit('message', lastMessages);
-  socket.on('newMessage', message => {
-    if (lastMessages.length < 5) {
-      lastMessages.push(message);
-    } else {
-      lastMessages.shift();
-      lastMessages.push(message);
-    }
-    broadcast(message);
+  io.sockets.emit('updateRoomsTab', rooms);
+
+  socket.on('createRoom', () => {
+    createNewRoom();
+    io.sockets.emit('updateRoomsTab', rooms);
+  });
+
+  socket.on('deleteRoom', roomName => {
+    deleteRoom(roomName);
+    io.sockets.emit('updateRoomsTab', rooms);
+  });
+
+  socket.on('joinRoom', (roomName, userName) => {
+    const messageObj = {
+      userName: 'NOTIFICATION CHAT',
+      message: `${userName} is connected to the chat.`,
+    };
+
+    roomIndex = rooms.indexOf(roomName);
+    socket.join(rooms[roomIndex]);
+    lastMessages.push(messageObj);
+    socket.emit('serverBroadcastPrevMessages', lastMessages);
+  });
+
+  socket.on('leaveRoom', (roomName, userName) => {
+    const messageObj = {
+      userName: 'NOTIFICATION CHAT',
+      message: `${userName} has left the chat.`,
+    };
+    sendMessageToServerAndBroadcastIt(roomName, messageObj);
+
+    roomIndex = rooms.indexOf(roomName);
+    socket.leave(rooms[roomIndex]);
+  });
+
+  socket.on('chatterSendAMessage', (messageObj, roomName) => {
+    sendMessageToServerAndBroadcastIt(roomName, messageObj);
   });
 });
 
